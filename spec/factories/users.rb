681fc30ad87676f826c1d848db4be62360ebf8e0
Carl Eqladios <ceqladios@gmail.com>
2018-08-23 13:33:25 +0200

    add username to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :user do
       email { Faker::Internet.email }
-      password { Faker::Lorem.word }
+      password { Faker::Lorem.word }, [6]
+      username { Faker::Lorem.word }
     end
   end
